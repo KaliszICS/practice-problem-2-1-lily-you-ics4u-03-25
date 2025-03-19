7e1399fdc8de5f23184451556c4c5765e69b7522
lily <lily.you@student.tdsb.on.ca>
2025-03-19 01:33:58 +0000

    Completed 2.1

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,34 @@ public class PracticeProblem {
 
 	}
 
+	public static int find(int[]array, int number){
+		for(int i = 0; i < array.length; i++){
+			if(array[i] == number){
+				return i;
+			}
+		}
+		return -1;
+	}
 	
+	public static int findLast(String[] array, String string){
+		for(int i = array.length-1; i >= 0; i--){
+			if(array[i].equals(string)){
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public static int findSecond(char[] array, char character){
+		int index = -1;
+		int count = 0;
+		for(int i = 0; i < array.length; i++){
+			if(array[i] == character && count < 2){
+				index = i;
+				count += 1;
+			}
+		}
+		return index;
+	}
 
 }
